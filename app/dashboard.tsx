f1eceb8e8e83200618f873df1e53cb6bfe35f222
Asadbek <raozas3@gmail.com>
2025-09-16 09:59:35 +0500

    Refactor DrawerPage to include customizable options and integrate Logout button

--- app/dashboard.tsx
+++ app/dashboard.tsx
@@ -9,12 +9,30 @@ import ReanimatedDrawerLayout, {
   DrawerPosition,
   DrawerType,
 } from "react-native-gesture-handler/ReanimatedDrawerLayout";
+import Button from "@/components/button";
+import { useRouter } from "expo-router";
 
 const Ava = () => {
-  return <Image source={bearAva} style={{ width: 50, height: 50, borderRadius: 25 }} />;
+  return (
+    <Image
+      source={bearAva}
+      style={{ width: 50, height: 50, borderRadius: 25 }}
+    />
+  );
 };
 
 const DrawerPage = () => {
+  const featherIcons = ["user", "settings", "help-circle"] as const;
+  type FeatherIconName = (typeof featherIcons)[number];
+
+  const Options: { name: string; icon: FeatherIconName }[] = [
+    { name: "Profile", icon: "user" },
+    { name: "Settings", icon: "settings" },
+    { name: "Help", icon: "help-circle" },
+  ];
+
+   const router = useRouter();
+
   return (
     <View
       style={{ flex: 1, justifyContent: "flex-start", alignItems: "center" }}
@@ -31,15 +49,58 @@ const DrawerPage = () => {
           backgroundColor: "#F5F9FE",
         }}
       >
-        <View style={{ flexDirection: "row", alignItems: "center", justifyContent: "space-between", width: "100%" }}>
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-between",
+            width: "100%",
+          }}
+        >
           <Ava />
           <Feather name="moon" size={24} color="#3461FD" />
         </View>
-        
-        <Text style={{ fontSize: 18, fontFamily: "Poppins-Bold", color: "#61677D" }}>
+
+        <Text
+          style={{ fontSize: 18, fontFamily: "Poppins-Bold", color: "#61677D" }}
+        >
           User Name
         </Text>
       </View>
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "center",
+          flexWrap: "wrap",
+          marginTop: 20,
+        }}
+      >
+        {Options.map((o, index) => (
+          <View
+            key={index}
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              padding: 15,
+              gap: 20,
+              width: "100%",
+            }}
+          >
+            <Feather name={o.icon} size={24} color="#3461FD" />
+            <Text
+              style={{
+                fontSize: 16,
+                fontFamily: "Poppins-Regular",
+                color: "#61677D",
+              }}
+            >
+              {o.name}
+            </Text>
+          </View>
+        ))}
+      </View>
+      <Button text="Logout" onPress={() => router.push("/(auth)/signIn")} width={280} type="refuse" style={{ bottom: 50, position: "absolute" }} />
     </View>
   );
 };
--- components/button.tsx
+++ components/button.tsx
@@ -6,12 +6,16 @@ interface ButtonProps {
   onPress?: () => void;
   text: string;
   type?: "default" | "refuse";
+  width?: number;
+  style?: object;
 }
 
 export default function Button({
   onPress,
   text,
   type = "default",
+  width,
+  style,
 }: ButtonProps) {
   return (
     <Pressable
@@ -19,6 +23,8 @@ export default function Button({
       style={[
         styles.button,
         { backgroundColor: type === "refuse" ? "#ef233c" : "#3461FD" },
+        width ? { width: width } : {},
+        style,
       ]}
     >
       <Text style={{ color: "white", fontSize: 16, fontFamily: "Poppins-Regular" }}>
